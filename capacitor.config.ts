0750999704e08b01641c6b850e400979d64c3640
isinning1 <i.sinning@uniandes.edu.co>
2026-03-11 10:02:50 -0500

    Se gestiona android para poder generar APK

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.binx.mobile',
+  appName: 'BINX Mobile',
+  webDir: 'www'
+};
+
+export default config;
