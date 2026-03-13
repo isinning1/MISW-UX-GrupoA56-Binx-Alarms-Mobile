18c378da110b3ad65cbb5d3bbbf4a2d984f58f2f
isinning1 <i.sinning@uniandes.edu.co>
2026-03-13 09:35:33 -0500

    implementacion de feat status

--- src/app/pages/alarm-status/alarm-status.page.ts
+++ src/app/pages/alarm-status/alarm-status.page.ts
@@ -1,20 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { checkmarkOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-alarm-status',
   templateUrl: './alarm-status.page.html',
   styleUrls: ['./alarm-status.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, IonicModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AlarmStatusPage implements OnInit {
+export class AlarmStatusPage {
+  alarmId = '';
+  status = 'attended';
+  alarmTitle = 'Sacar al perro';
+  statusMessage = '¡Atendida!';
 
-  constructor() { }
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) {
+    addIcons({
+      checkmarkOutline,
+    });
 
-  ngOnInit() {
+    this.alarmId = this.route.snapshot.paramMap.get('id') ?? '';
+    this.status = this.route.snapshot.paramMap.get('state') ?? 'attended';
+
+    this.applyStatusPresentation();
   }
 
+  private applyStatusPresentation(): void {
+    switch (this.status) {
+      case 'attended':
+      default:
+        this.statusMessage = '¡Atendida!';
+        break;
+    }
+  }
+
+  onContinue(): void {
+    void this.router.navigateByUrl('/alarms');
+  }
 }
